5775ca93130463ce56cc58742e0bada715f3ddd5
tdark23 <tdark237@gmail.com>
2021-08-21 03:23:11 +0100

    Navbar terminée

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <h2>Mon portfolio...</h2>
+    <Navbar/>
   );
 }
 
